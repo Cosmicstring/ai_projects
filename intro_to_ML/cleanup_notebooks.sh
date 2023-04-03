d706233fa6787cf08d3b359d8f579a344f76e946
Andrija Kostic <andrii.kostic@gmail.com>
2023-04-03 14:34:50 +0200

    utilities

---
+++ intro_to_ML/cleanup_notebooks.sh
@@ -0,0 +1,3 @@
+#/bin/bash -l
+
+git config filter.strip-notebook-output.clean 'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --to=notebook --stdin --stdout --log-level=ERROR'
---
+++ intro_to_ML/datasets/get_datasets.sh
@@ -0,0 +1,4 @@
+#/bin/bash -l
+
+kaggle datasets download "dansbecker/melbourne-housing-snapshot" --unzip
+kaggle datasets download "dansbecker/home-data-for-ml-course" --unzip
